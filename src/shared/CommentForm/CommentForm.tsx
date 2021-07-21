8b92683b5c22423b4b4e09e67ab0c15fe7d40499
t-kovik <tatyana.kovik@icloud.com>
2021-07-21 15:18:06 +0300

    add easy-peasy state

--- src/shared/CommentForm/CommentForm.tsx
+++ src/shared/CommentForm/CommentForm.tsx
@@ -1,7 +1,7 @@
-import React, {ChangeEvent, FormEvent, useEffect, useRef} from 'react';
+import React, {ChangeEvent, FormEvent, useEffect, useRef, useState} from 'react';
 import styles from './commentform.scss';
 import {Button} from "../Button";
-import {atom, RecoilRoot, useRecoilState} from "recoil";
+import { Action } from 'easy-peasy';
 
 interface IProps {
     author?: string;
@@ -10,36 +10,48 @@ interface IProps {
     onSubmit?: (ev: FormEvent) => void;
 }
 
+
+
+interface CommentsModel {
+    comments: string;
+    addComment: Action<CommentsModel, string>;
+}
+
+import { createTypedHooks } from 'easy-peasy';
+
+const typedHooks = createTypedHooks<CommentsModel>();
+
+const useStoreActions = typedHooks.useStoreActions;
+const useStoreDispatch = typedHooks.useStoreDispatch;
+const useStoreState = typedHooks.useStoreState;
+
 export function CommentForm({author, value, onChange, onSubmit}: IProps) {
     const ref = useRef<HTMLTextAreaElement>(null);
 
+    const [comment, setComment] = useState('');
+
+    const todos = useStoreState((state) => state.comments);
+    const addComments = useStoreActions((actions) => actions.addComment);
+
     useEffect(() => {
         ref.current?.focus();
+        setComment(todos);
     }, []);
 
-    const textState = atom({
-        key: 'textState',
-        default: '',
-    });
 
-    function TextInput() {
-        const [text, setText] = useRecoilState(textState);
-
-        return (
-            <textarea className={styles.input}
-                      value={text}
-                      ref={ref}
-                      onChange={(ev) => setText(ev.target.value)}/>
-        );
-    }
 
     return (
         <div>
             <form className={styles.form}>
-                <RecoilRoot>
-                    <TextInput/>
-                </RecoilRoot>
-                <Button className={styles.btnSubmit} isIcon={false} text={'Комментировать'}/>
+                     <textarea className={styles.input}
+                               value={comment}
+                         //defaultValue={`${author}, `}
+                               onChange={e => setComment(e.target.value)}
+                               ref={ref} />
+                <Button className={styles.btnSubmit} isIcon={false} text={'Комментировать'} onClick={(ev) => {
+                    ev.preventDefault()
+                    addComments(comment)
+                }}/>
             </form>
         </div>
     )
--- src/shared/CommentFormContainer/CommentFormContainer.tsx
+++ src/shared/CommentFormContainer/CommentFormContainer.tsx
@@ -3,6 +3,15 @@ import {useDispatch, useSelector} from "react-redux";
 import {RootState, UpdateComment} from "../../store/reducer"
 import {CommentForm} from "../CommentForm";
 import {CommentFormNew} from "../CommentFormNew";
+import {action, createStore, StoreProvider} from "easy-peasy";
+
+const store = createStore({
+    comments: 'Hello, easy-peasy',
+    addComment: action((state, comment) => {
+        // @ts-ignore
+        state.comments = comment;
+    }),
+});
 
 export function CommentFormContainer() {
     //const value = useSelector<RootState, string>(state => state.commentText);
@@ -28,7 +37,9 @@ export function CommentFormContainer() {
 
     return (
         <div>
-            <CommentForm />
+            <StoreProvider store={store}>
+                <CommentForm />
+            </StoreProvider>
                 {/*<CommentFormNew />*/}
         </div>
 
